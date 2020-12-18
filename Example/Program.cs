64d3906a8126ebcc503c9f334b6c8bba8d75c7d4
Jon Tiller <jon.w.tiller@gmail.com>
2020-12-18 20:02:53 +0100

    saves images to folder, and cleans up every time its run.

--- Example/Program.cs
+++ Example/Program.cs
@@ -13,13 +13,22 @@ namespace Example
         private static readonly YoloV3Onnx _yoloV3Onnx = new YoloV3Onnx(Path.Combine("Model","yolov3.onnx"),Path.Combine("Model","labels.txt"));
         static void Main(string[] args)
         {
+            if (!Directory.Exists("result"))
+            {
+                Directory.CreateDirectory("result");
+            }
+            foreach (var file in Directory.GetFiles("result"))
+            {
+                File.Delete(file);
+            }
+
             foreach (var image in Directory.GetFiles("images"))
             {
                 var skImage = SKImage.FromEncodedData(new MemoryStream(File.ReadAllBytes(image)));
                 var result = _yoloV3Onnx.Detect(skImage);
 
                 var skImageResult = skImage.Resize(Constants.YoloImage.Width,Constants.YoloImage.Height).DrawRectangles(result);
-                skImageResult.SaveToFile($"{Guid.NewGuid()}.png");
+                skImageResult.SaveToFile(Path.Combine("result",$"{Guid.NewGuid()}.png"));
             }
         }
     }
